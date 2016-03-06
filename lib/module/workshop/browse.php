8e0254f0348e8fee598b45ac7e01f465226726a2
Pavel Žák <just.paja@gmail.com>
2016-03-06 23:16:37 +0100

    Workshop page

---
+++ lib/module/workshop/browse.php
@@ -0,0 +1,23 @@
+<?
+
+namespace Module\Workshop
+{
+	class Browse extends \System\Module
+	{
+		public function run()
+		{
+			$items = \Workshop::get_all()->fetch();
+
+			for ($i = count($items); $i<8; $i++) {
+				$items[] = new \Workshop(array(
+					"name" => "Připravujeme",
+					"desc" => 'Workshop '.($i + 1),
+				));
+			}
+
+			$this->partial('pages/workshops', array(
+				"items" => $items,
+			));
+		}
+	}
+}
