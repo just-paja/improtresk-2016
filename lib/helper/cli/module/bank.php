0c7ec46e78eaa58d44b55009e29f56953d95d994
Pavel Žák <pavel@berrycloud.cz>
2016-03-26 11:24:12 +0100

    CLI helpers

---
+++ lib/helper/cli/module/bank.php
@@ -0,0 +1,57 @@
+<?
+
+namespace Helper\Cli\Module
+{
+	class Bank extends \Helper\Cli\Module
+	{
+		const URL_TRANSACTIONS = 'https://www.fio.cz/ib_api/rest/periods/{token}/{from}/{to}/transactions.json';
+
+		protected static $info = array(
+			'name' => 'bank',
+			'head' => array(
+				'Manage your account transactions',
+			),
+		);
+
+
+		protected static $attrs = array(
+			"help"    => array("type" => 'bool', "value" => false, "short" => 'h', "desc"  => 'Show this help'),
+			"verbose" => array("type" => 'bool', "value" => false, "short" => 'v', "desc" => 'Be verbose'),
+		);
+
+
+		protected static $commands = array(
+			"pair" => array('Pair transactions'),
+		);
+
+
+		public function cmd_pair()
+		{
+			\System\Init::full();
+
+			$token = \System\Settings::get('bank', 'token');
+			$from  = \System\Settings::get('bank', 'from');
+			$to    = date('Y-m-d');
+
+			$url = str_replace('{token}', $token, self::URL_TRANSACTIONS);
+			$url = str_replace('{from}', $from, $url);
+			$url = str_replace('{to}', $to, $url);
+
+			$res = \Helper\Offcom\Request::get($url);
+
+			if (!$res->ok()) {
+				if ($res->status == 409) {
+					\Helper\Cli::out('Please wait 30 seconds and try again.');
+					exit(4);
+				} else {
+					\Helper\Cli::out('Unknown error during transaction scrape.');
+					exit(5);
+				}
+			}
+
+			$feed = \System\Json::decode($res->content);
+			\Workshop\Payment::pair_with_feed($feed);
+		}
+	}
+}
+
---
+++ lib/helper/cli/module/notif.php
@@ -0,0 +1,176 @@
+<?
+
+namespace Helper\Cli\Module
+{
+	class Notif extends \Helper\Cli\Module
+	{
+		protected static $info = array(
+			'name' => 'bank',
+			'head' => array(
+				'Manage your account transactions',
+			),
+		);
+
+
+		protected static $attrs = array(
+			"help"    => array("type" => 'bool', "value" => false, "short" => 'h', "desc"  => 'Show this help'),
+			"verbose" => array("type" => 'bool', "value" => false, "short" => 'v', "desc" => 'Be verbose'),
+		);
+
+
+		protected static $commands = array(
+			"general" => array('Send notification containing general information'),
+			"lunch"   => array('Send notification about lunch picker'),
+			"match"   => array('Send notification about match survey'),
+			"camp"    => array('Send notification about summer impro camp 2015'),
+		);
+
+
+		public function cmd_lunch()
+		{
+			\System\Init::full();
+
+			$users = \Workshop\SignUp::get_all()
+				->where(array(
+					"sent_lunch" => false,
+					"solved" => true,
+					"lunch" => true
+				))
+				->fetch();
+
+			\Helper\Cli::do_over($users, function($key, $user) {
+				$ren = new \System\Template\Renderer\Txt();
+				$ren->reset_layout();
+				$ren->partial('mail/notif/lunch', array(
+					"user"   => $user,
+					"symvar" => $user->check->symvar
+				));
+
+				$mail = new \Helper\Offcom\Mail(array(
+					'rcpt'     => array($user->email),
+					'subject'  => 'Improtřesk 2015 - Výběr oběda',
+					'reply_to' => \System\Settings::get('offcom', 'default', 'reply_to'),
+					'message'  => $ren->render_content()
+				));
+
+				$mail->send();
+
+				$user->sent_lunch = true;
+				$user->save();
+			});
+		}
+
+
+		public function cmd_match()
+		{
+			\System\Init::full();
+
+			$users = \Workshop\SignUp::get_all()
+				->where(array(
+					"sent_match" => false,
+					"solved" => true
+				))
+				->fetch();
+
+			\Helper\Cli::do_over($users, function($key, $user) {
+				$ren = new \System\Template\Renderer\Txt();
+				$ren->reset_layout();
+				$ren->partial('mail/notif/match', array(
+					"user"   => $user,
+					"symvar" => $user->check->symvar
+				));
+
+				$mail = new \Helper\Offcom\Mail(array(
+					'rcpt'     => array($user->email),
+					'subject'  => 'Improtřesk 2015 - Týmy na zápas',
+					'reply_to' => \System\Settings::get('offcom', 'default', 'reply_to'),
+					'message'  => $ren->render_content()
+				));
+
+				$mail->send();
+
+				$user->sent_match = true;
+				$user->save();
+			});
+		}
+
+
+		public function cmd_general()
+		{
+			\System\Init::full();
+
+			$users = \Workshop\SignUp::get_all()
+				->where(array(
+					"sent_general" => false,
+					"solved" => true
+				))
+				->add_filter(array(
+					"attr"    => 'id_assigned_to',
+					"type"    => 'is_null',
+					"is_null" => false
+				))
+				->fetch();
+
+			\Helper\Cli::do_over($users, function($key, $user) {
+				$ren = new \System\Template\Renderer\Txt();
+				$ren->reset_layout();
+				$ren->partial('mail/notif/general', array(
+					"user"   => $user,
+					"symvar" => $user->check->symvar
+				));
+
+				$mail = new \Helper\Offcom\Mail(array(
+					'rcpt'     => array($user->email),
+					'subject'  => 'Improtřesk 2015 - To nejdůležitější',
+					'reply_to' => \System\Settings::get('offcom', 'default', 'reply_to'),
+					'message'  => $ren->render_content()
+				));
+
+				$mail->send();
+
+				$user->sent_general = true;
+				$user->save();
+			});
+		}
+
+
+		public function cmd_camp()
+		{
+			\System\Init::full();
+
+			$users = \Workshop\SignUp::get_all()
+				->where(array(
+					"sent_camp" => false,
+					"solved" => true
+				))
+				->add_filter(array(
+					"attr"    => 'id_assigned_to',
+					"type"    => 'is_null',
+					"is_null" => false
+				))
+				->fetch();
+
+			\Helper\Cli::do_over($users, function($key, $user) {
+				$ren = new \System\Template\Renderer\Txt();
+				$ren->reset_layout();
+				$ren->partial('mail/after/improcamp', array(
+					"user"   => $user,
+					"symvar" => $user->check->symvar
+				));
+
+				$mail = new \Helper\Offcom\Mail(array(
+					'rcpt'     => array($user->email),
+					'subject'  => 'Improtřesk 2015 - Pozvánka na letní IMPRO CAMP 2015 se slevou',
+					'reply_to' => \System\Settings::get('offcom', 'default', 'reply_to'),
+					'message'  => $ren->render_content()
+				));
+
+				$mail->send();
+
+				$user->sent_camp = true;
+				$user->save();
+			});
+		}
+	}
+}
+
