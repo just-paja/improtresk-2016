2a71afb130632146e7154922f544007a42861fa1
Pavel Žák <pavel@berrycloud.cz>
2016-04-01 10:04:33 +0200

    Fix start date

--- lib/module/forms/signup.php
+++ lib/module/forms/signup.php
@@ -98,7 +98,7 @@ namespace Module\Forms
 
 		public function run()
 		{
-			$start = new \DateTime("2016-04-01 10:00:00+01:00");
+			$start = new \DateTime("2016-04-01 09:00:00+01:00");
 			$end   = new \DateTime("2016-05-12 00:00:00+01:00");
 			$now   = new \DateTime();
 
