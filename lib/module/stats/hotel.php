93f967c78c4c61fb1031284427e8bfc7658bcfbf
Pavel Žák <pavel@berrycloud.cz>
2016-04-30 03:29:12 +0200

    Hotel stats

--- lib/module/stats/hotel.php
+++ lib/module/stats/hotel.php
@@ -6,38 +6,33 @@ namespace Module\Stats
   {
     public function run()
     {
-      $total = \Workshop\Check::get_all()
-          ->add_cols(array('SUM(amount) as `total`'))
-          ->where(array(
-            "hotel" => true,
-            "solved" => true,
-          ))
-          ->assoc_with(null)
-          ->fetch_one();
+      $total = \Workshop\SignUp::count_all(array(
+        "hotel" => true,
+        "solved" => true,
+      ));
 
-      $totalChecked = \Workshop\Roommate::count();
-      $singles = array();
+      $totalChecked = \Workshop\Roommate::count_all();
       $matched = array();
       $used = array();
       $rooms = array();
-      $skipped = array();
       $checked = \Workshop\Roommate::get_all()->fetch();
 
       foreach ($checked as $room) {
         $roommates = \Workshop\Roommate::get_all()->where(array(
-          "roommate" => $room->signup,
-          "signup" => $room->roommate,
+          "id_roommate" => $room->signup->id,
+          "id_signup" => $room->roommate->id,
         ))->fetch();
 
         if (!$roommates) {
-          $singles[] = $room->signup;
+          $matched[$room->signup->id] = array($room->signup->id);
         } else {
-          if (!$matched[$room->signup->id]) {
+          if (!isset($matched[$room->signup->id])) {
             $matched[$room->signup->id] = array($room->signup->id);
           }
 
           array_push($matched[$room->signup->id], $room->roommate->id);
-          $matched[$room->signup->id] = sort($matched[$room->signup->id]);
+          sort($matched[$room->signup->id]);
+          $matched[$room->signup->id] = array_unique($matched[$room->signup->id]);
         }
       }
 
@@ -58,26 +53,34 @@ namespace Module\Stats
 
           if ($checked == count($room)) {
             $rooms[] = $room;
-          } else {
-            $skipped[] = $room;
           }
         }
       }
 
-      v($singles);
-      v($rooms);
-      v($skipped);
-      exit;
+      $allMatched = array();
 
-      $stats = array(
+      foreach ($rooms as $i => $room) {
+        foreach ($room as $index => $id) {
+          $allMatched[] = $id;
+          $room[$index] = \Workshop\SignUp::find($id);
+        }
+
+        $rooms[$i] = $room;
+      }
+
+      $unMatched = \Workshop\SignUp::get_all()
+        ->where(array(
+          ' `id_workshop_signup` NOT IN (' . implode(',', $allMatched) . ')',
+          "hotel" => true,
+          "solved" => true,
+        ))
+        ->fetch();
+
+      $this->partial('stats/hotel', array(
         "total" => $total,
         "totalChecked" => $totalChecked,
-        "checked"   => $checked,
-      );
-
-      $this->partial('stats/signups', array(
-        "stats" => $stats,
-        "workshops" => $ws,
+        "rooms" => $rooms,
+        "unMatched" => $unMatched,
       ));
     }
   }
